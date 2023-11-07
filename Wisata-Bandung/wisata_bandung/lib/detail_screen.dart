35fd62c93211c0415dc51fa9370d7dc719067847
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-07 20:11:52 +0700

    detail screen for web view

--- Wisata-Bandung/wisata_bandung/lib/detail_screen.dart
+++ Wisata-Bandung/wisata_bandung/lib/detail_screen.dart
@@ -11,110 +11,193 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    Image.asset(place.imageAsset),
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          CircleAvatar(
-                            backgroundColor: Colors.grey,
-                            child: IconButton(
-                              icon: const Icon(
-                                Icons.arrow_back,
-                                color: Colors.white,
-                              ),
-                              onPressed: () => Navigator.pop(context),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 32.0,
+        ),
+        child: Center(
+          child: SizedBox(
+            width: 1200,
+            child: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+              if (constraints.maxWidth > 800) {
+                return DetailWebPage(place);
+              } else {
+                return DetailMobileScreen(place);
+              }
+            }),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailMobileScreen extends StatelessWidget {
+  final TourismPlace place;
+  const DetailMobileScreen(this.place, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Image.asset(place.imageAsset),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          child: IconButton(
+                            icon: const Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
                             ),
+                            onPressed: () => Navigator.pop(context),
                           ),
-                          const CircleAvatar(
-                            backgroundColor: Colors.grey,
-                            child: Favoritebutton(),
-                          )
-                        ],
-                      ),
+                        ),
+                        const CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          child: Favoritebutton(),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16),
+                child: Text(
+                  place.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontFamily: 'Staatliches'),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        const Icon(
+                          Icons.calendar_today,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          place.openDays,
+                          style: informationTextStyle,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        const Icon(
+                          Icons.access_time,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(place.openTime, style: informationTextStyle),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        const Icon(
+                          Icons.monetization_on,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(place.ticketPrice, style: informationTextStyle),
+                      ],
                     ),
                   ],
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 16),
-                  child: Text(
-                    place.name,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontFamily: 'Staatliches'),
+              ),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  place.description,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.white,
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          const Icon(
-                            Icons.calendar_today,
-                            color: Colors.white,
-                          ),
-                          const SizedBox(
-                            height: 8.0,
-                          ),
-                          Text(
-                            place.openDays,
-                            style: informationTextStyle,
-                          ),
-                        ],
+              ),
+              SizedBox(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    for (var url in place.imageUrls)
+                      Container(
+                        padding: const EdgeInsets.all(4.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.network(url),
+                        ),
                       ),
-                      Column(
-                        children: <Widget>[
-                          const Icon(
-                            Icons.access_time,
-                            color: Colors.white,
-                          ),
-                          const SizedBox(
-                            height: 8.0,
-                          ),
-                          Text(place.openTime, style: informationTextStyle),
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          const Icon(
-                            Icons.monetization_on,
-                            color: Colors.white,
-                          ),
-                          const SizedBox(
-                            height: 8.0,
-                          ),
-                          Text(place.ticketPrice, style: informationTextStyle),
-                        ],
-                      ),
-                    ],
-                  ),
+                  ],
                 ),
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    place.description,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                SizedBox(
+              )
+            ]),
+      ),
+    );
+  }
+}
+
+class DetailWebPage extends StatelessWidget {
+  final TourismPlace place;
+  const DetailWebPage(this.place, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      const Text(
+        "Wisata Bandung",
+        style: TextStyle(
+          fontFamily: 'Staatliches',
+          fontSize: 32,
+          color: Colors.white,
+        ),
+      ),
+      const SizedBox(
+        height: 32,
+      ),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+              child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(place.imageAsset),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
                   height: 150,
+                  padding: const EdgeInsets.only(bottom: 16),
                   child: ListView(
                     scrollDirection: Axis.horizontal,
                     children: [
@@ -127,12 +210,87 @@ class DetailScreen extends StatelessWidget {
                           ),
                         ),
                     ],
-                  ),
-                )
-              ]),
-        ),
-      ),
-    );
+                  ))
+            ],
+          )),
+          const SizedBox(width: 32),
+          Expanded(
+            child: Card(
+              color: const Color.fromARGB(221, 15, 15, 15),
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Text(
+                      place.name,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: 'Staatliches'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: <Widget>[
+                            const Icon(Icons.calendar_today, color: Colors.white),
+                            const SizedBox(width: 8.0),
+                            Text(
+                              place.openDays,
+                              style: informationTextStyle,
+                            ),
+                          ],
+                        ),
+                        const CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          child: Favoritebutton(),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        const Icon(Icons.access_time, color: Colors.white),
+                        const SizedBox(width: 8.0),
+                        Text(
+                          place.openTime,
+                          style: informationTextStyle,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8.0),
+                    Row(
+                      children: <Widget>[
+                        const Icon(Icons.monetization_on, color: Colors.white),
+                        const SizedBox(width: 8.0),
+                        Text(
+                          place.ticketPrice,
+                          style: informationTextStyle,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text(
+                        place.description,
+                        textAlign: TextAlign.justify,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontFamily: 'Oxygen',
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      )
+    ]);
   }
 }
 
