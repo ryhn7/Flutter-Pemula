249fd81d8af9555d5c0e5969b7fa01a032a14c65
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-07 19:34:56 +0700

    home screen view for web

--- Wisata-Bandung/wisata_bandung/lib/home_screen.dart
+++ Wisata-Bandung/wisata_bandung/lib/home_screen.dart
@@ -8,53 +8,123 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          title: const Text("Wisata Bandung"),
+          title: const Text(
+              "Wisata Bandung"),
         ),
-        body: ListView.builder(
-            itemCount: tourismPlaceList.length,
-            itemBuilder: (context, index) {
-              final TourismPlace place = tourismPlaceList[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return DetailScreen(place);
-                  }));
-                },
-                child: Card(
-                  color: const Color.fromARGB(221, 15, 15, 15),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Expanded(flex: 1, child: Image.asset(place.imageAsset)),
-                      Expanded(
-                          flex: 2,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Text(
-                                  place.name,
-                                  style: const TextStyle(
-                                      fontSize: 16.0, color: Colors.white),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  place.location,
-                                  style: const TextStyle(color: Colors.white),
-                                )
-                              ],
+        body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            return const TourismPlaceList();
+          } else if (constraints.maxWidth <= 1200) {
+            return const TourismPlaceGrid(gridCount: 4);
+          } else {
+            return const TourismPlaceGrid(gridCount: 6);
+          }
+        }));
+  }
+}
+
+class TourismPlaceList extends StatelessWidget {
+  const TourismPlaceList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: tourismPlaceList.length,
+        itemBuilder: (context, index) {
+          final TourismPlace place = tourismPlaceList[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen(place);
+              }));
+            },
+            child: Card(
+              color: const Color.fromARGB(221, 15, 15, 15),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(flex: 1, child: Image.asset(place.imageAsset)),
+                  Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              place.name,
+                              style: const TextStyle(
+                                  fontSize: 16.0, color: Colors.white),
                             ),
-                          ))
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              place.location,
+                              style: const TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ))
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
+
+class TourismPlaceGrid extends StatelessWidget {
+  final int gridCount;
+  const TourismPlaceGrid({Key? key, required this.gridCount}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: GridView.count(
+        crossAxisCount: gridCount,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+        children: <Widget>[
+          for (var place in tourismPlaceList)
+            InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return DetailScreen(place);
+                }));
+              },
+              child: Card(
+                  color: const Color.fromARGB(221, 15, 15, 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: Image.asset(place.imageAsset, fit: BoxFit.cover),
+                      ),
+                      const SizedBox(height: 8),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                place.name,
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                              Text(place.location,
+                                  style: const TextStyle(color: Colors.white))
+                            ]),
+                      )
                     ],
-                  ),
-                ),
-              );
-            }));
+                  )),
+            )
+        ],
+      ),
+    );
   }
 }
--- Wisata-Bandung/wisata_bandung/lib/main.dart
+++ Wisata-Bandung/wisata_bandung/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Wisata Bandung',
       theme: ThemeData(
         // This is the theme of your application.
@@ -31,7 +32,6 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
-
       ),
       home: const HomeScreen(),
     );
